fa3ed9465c51fce6eca68a2bc3a45b58d98fdea0
lambda-tuber <crypto.tuber.magika@gmail.com>
2023-10-08 10:59:09 +0900

    09 Haskell ServantでWEBアプリ作成

--- azhaskell-func-handler/custom-handler/app/Main.hs
+++ azhaskell-func-handler/custom-handler/app/Main.hs
@@ -1,4 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
+import Control.Monad.Except
+import Control.Monad.IO.Class
+
+import qualified Data.Text as T
+
+import Network.Wai.Middleware.RequestLogger (logStdout)
+import Servant.Server
+
+import CustomHandlerIF.API
+
+-- |
+--
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Hello, Haskell!"
+
+  let config = Config "http://127.0.0.1/"
+      middlewares = logStdout
+      server = CustomHandlerIFBackend healthCheckHandler
+
+  runCustomHandlerIFMiddlewareServer config middlewares server
+
+
+-- |
+--
+healthCheckHandler :: (ExceptT ServerError IO) T.Text
+healthCheckHandler = do
+  liftIO $ putStrLn "healthCheckHandler called."
+
+  return $ "health check ok. status:200"
+
